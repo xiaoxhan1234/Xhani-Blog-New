04c6c8b98ceb4ec54b352d96595b017727b53d39
xiaoxhan1234 <3084058513@qq.com>
2026-09-02 13:39:38 +0800

    Update 1 file: announcementConfig.ts

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -9,13 +9,13 @@ export const announcementConfig: AnnouncementConfig = withUserConfig(
 	"announcement",
 	{
 		title: "", // 公告标题，填空使用 i18n 字符串 Key.announcement
-		content: "The only way to do great work is to love what you do", // 公告内容
+		content: "使用shirone最新版博客模板，速度嘎嘎快", // 公告内容
 		closable: true, // 允许用户关闭公告
 		link: {
 			enable: true, // 启用链接
-			text: "GitHub", // 链接文本
-			url: "https://github.com", // 链接 URL
-			external: true, // 外部链接
+			text: "相册", // 链接文本
+			url: "/albums/", // 链接 URL
+			external: false, // 外部链接
 		},
 	},
 );
